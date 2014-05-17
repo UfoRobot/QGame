cde298add695251d79d52f820f02f4d8a2f09666
AscanioFX <ascanio.vimercati@gmail.com>
2014-05-17 12:28:10 +0200

    Debugged stuff, added setting class

--- q.py
+++ q.py
@@ -6,6 +6,33 @@
 # Class definition
 #
 
+class Settings():
+    """ A settings class. It stores number of players and their custom symbols.
+        Default is 2 players: X and O """
+    
+    #
+    # Class members
+    #
+
+    self.nPlayers = 0   # Number of players
+    self.players = {}   # Associates player's id and player's symbol
+
+    #
+    # Class methods
+    #
+
+    def __init__(self):
+        # Sets default settings values
+        self.nPlayers = 2
+        self.players = {1:"X", 2:"O"}
+
+    def setPlayers():
+        # Sets custom setting values. More player and custom symbols
+        self.nPlayers = raw_imput("Enter number of players:")
+        for player in range(1, nPlayers+1):
+            symbol = raw_imput("Choose a symbol for player n {}:".format(player))
+            self.players[player] = symbol
+
 class QField():
     """ A field for the Q-Game and all methods needed to play """
 
@@ -13,23 +40,26 @@ class QField():
     # Class members
     #
 
-    n = 0
-    m = 0
-
+    n = 0        # Number of rows
+    m = 0        # Number of columns
+    field = []   # Field is a list of lists
 
     #
     # Class methods
     #
     
-    # init builds up a clear field n x m
     def __init__(self, rows, columns):
-        n = rows
-        m = columns
+    # init builds up a clear field n x m
+        self.n = rows
+        self.m = columns
         self.field = [] 
         for row in range(n):
             self.field.append([0 for x in range(m)])
 
-    def checkSequence(self, sequence):
+    def show(self):
+        for i in range
+
+    def __checkSequence(self, sequence):
         """ Given a sequence returns the element that occurs 4 times
             in a row. If more the first, if none None """
 
@@ -45,62 +75,66 @@ class QField():
                 return mayWin
         return None
 
-    def checkRows(self, field = None):
-        """ Checks for a winner on field's rows. May be passed a different field """
-        if field == None:
-            field = self.field
-        for row in field:
-            mayWin = self.checkSequence(row)
+    def __checkRows(self):
+        """ Checks for a winner on field's rows """
+
+        for row in self.field:
+            mayWin = self.__checkSequence(row)
             if mayWin != None:
                 return mayWin
 
-    def checkColumns(self, field = None):
-        if field == None:
-            field = self.field
-        column = []
+    def __checkColumns(self):
+        """ You can get that...."""
 
-        for j in range(len(field[0])):
-            for i in range(len(field)):
-                column.append(field[i][j])
-            print column
-            mayWin = self.checkSequence(column)
+        column = []
+        for j in range(self.n):
+            for i in range(self.m)
+                column.append(self.field[i][j])
+            mayWin = self.__checkSequence(column)
             if mayWin != None:
                 return mayWin
             else:
                 column = []
         return None
 
-    def checkBlock(self, field = None):
-        if field == None:
-            field = self.field
-        block = []
+    def __checkBlocks(self):
+        """ Checks for a winning block """
 
-        for i in range(len(field)-1):
-            for j in range(len(field[0])-1):
+        block = []
+        for i in range(self.m-1):
+            for j in range(self.n-1):
                 block.append(field[i][j])
                 block.append(field[i][j+1])
                 block.append(field[i+1][j])
                 block.append(field[i+1][j+1])
-                print block
-                mayWin = self.checkSequence(block)
+                mayWin = self.__checkSequence(block)
                 if mayWin != None:
                     return mayWin
                 block = []
         return None
 
     def checkWin(self, field = None):
+        """ Checks for a victor on the field. May be passed a differet field for testing """
         if field == None:
             field = self.field
-        rowWinner = self.checkRows(field)
+        rowWinner = self.__checkRows(field)
         if rowWinner != None:
             return rowWinner
-        columnWinner = self.checkColumns(field)
+        columnWinner = self.__checkColumns(field)
         if columnWinner != None:
             return columnWinner
-        blockWinner = self.checkColumn(field)
+        blockWinner = self.__checkBlocks(field)
         if blockWinner != None:
             return blockWinner
         return None
+
+    def move(self, player, i, j):
+        if self.field[i][j] != 0:
+            print "ERROR # QField.move --> Not an empty cell!"
+        else:
+            self.field[i][j] = player
+
+
 
 
 
